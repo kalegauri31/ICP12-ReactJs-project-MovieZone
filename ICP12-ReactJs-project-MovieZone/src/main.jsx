bff27ed5a6965cefe6b23f623abd52b5218525e7
kale gauri 31 <kalegauri2024@gmail.com>
2026-01-05 21:43:50 +0530

    intial commit

--- ICP12-ReactJs-project-MovieZone/src/main.jsx
+++ ICP12-ReactJs-project-MovieZone/src/main.jsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter,Routes,Route} from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+import Home from './Views/Home.jsx';
+import Booking from './Views/Booking.jsx';
+import About from './Views/About.jsx';
+import Login from './Views/Login.jsx';
+import Movie from './Views/Movie.jsx';
+import Payment from './Views/Payment.jsx';
+import Register from './Views/Register.jsx';
+import SeatSelection from './Views/SeatSelection.jsx';
+
+
+const root = document.getElementById("root");
+root.render(<BrowserRouter>
+<Routes>
+    <Route path="/" element={<h1>HOME</h1>}/>
+    <Route path="/About" element={<h1>About</h1>}/>
+    <Route path="/" element={<h1>Booking</h1>}/>
+    <Route path="/" element={<h1>Login</h1>}/>
+    <Route path="/" element={<h1>Movie</h1>}/>
+    <Route path="/" element={<h1>Payment</h1>}/>
+    <Route path="/" element={<h1>Register</h1>}/>
+    <Route path="/" element={<h1>SeatSelection</h1>}/>
+</Routes></BrowserRouter>);
