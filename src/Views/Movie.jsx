7337ae1b96b228d848d90e6c2441dc4ebceac253
siddhikharde <siddhikharde2005@gmail.com>
2026-01-05 23:50:46 +0530

    movie type selection done

--- src/Views/Movie.jsx
+++ src/Views/Movie.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import { useState } from 'react';
 
+const SelectBox = ({ value, onChange, options }) => {
+    return (
+        <select className='border px-5 py-2 w-[100%] cursor-pointer md:w-[25%] bg-[#234C6A] focus:outline-none rounded-[4px] border-[1.5px] text-gray-300 border-[#456882] '
+            value={value} onChange={onChange}>
+            {
+                options.map((item) => {
+                    return (
+                        <option key={item} value={item}>{item}</option>
+                    )
+                })
+            }
+        </select>
+    )
+}
+
 function Movie() {
     const [searchMovies, setSearchMovies]= useState("");
+     const [movieType, setMovieType] = useState("All");
     return (
         <div className='bg-[#1B3C53] min-h-screen'>
             <div className='flex flex-col w-[100%]  md:flex-row gap-5 items-center justify-center  p-10 '>
@@ -12,6 +28,12 @@ function Movie() {
                             setSearchMovies(e.target.value)
                         }} className='border px-5 w-[100%] py-2 hover:scale-3d text-gray-300 focus:outline-none rounded-[4px] border-[1.5px]  border-[#456882]' />
                 </div>
+
+                 <SelectBox
+                        value={movieType}
+                        onChange={(e) => setMovieType(e.target.value)}
+                        options={["All", "Action", "Comedy", "Drama", "Horror", "Romance", "Thriller"]}
+                    />
             </div>
         </div>
     )
