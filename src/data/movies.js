da4001a2874cf54950c92587deba74ff9105c407
siddhikharde <siddhikharde2005@gmail.com>
2026-01-06 12:45:59 +0530

    movies data added

---
+++ src/data/movies.js
@@ -0,0 +1,111 @@
+
+
+const movies=[
+    {
+        image:"https://m.media-amazon.com/images/M/MV5BNjY5OTg4NTYtZjVkZS00YTZmLWIwNDEtM2Y0ODQyMzM2NTJiXkEyXkFqcGc@._V1_.jpg",
+        name:"War 2",
+        type:"Action",
+        language:"Hindi",
+        year:"2025",
+        duration:"2h 5min"
+    },{
+         image:"https://images.unsplash.com/photo-1594909122845-11baa439b7bf?w=400&h=600&fit=crop",
+        name:"Dhurandhar",
+        type:"Action",
+        language:"Hindi",
+        year:"2025",
+        duration:"1h 45 min"
+    },
+    {
+         image:"https://images.unsplash.com/photo-1514306191717-452ec28c7814?w=400&h=600&fit=crop",
+        name:"Sitaare Zameen Par",
+        type:"Comedy",
+        language:"Hindi",
+        year:"2025",
+        duration:"1h 48min"
+    },
+      {
+    image: "https://upload.wikimedia.org/wikipedia/en/9/95/Avatar_Fire_and_Ash_poster.jpeg",
+    name: "Avatar 3",
+    type: "Action",
+    language: "English",
+    year: "2025",
+    duration:"1h 52min"
+  },{
+    image: "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?w=400&h=600&fit=crop",
+    name: "Midnight in Paris",
+    type: "Romance",
+    language: "English",
+    year: "2024",
+    duration:"2h 56min"
+  },
+    {
+    image: "https://c.saavncdn.com/992/Bad-Newz-Hindi-2024-20250730113701-500x500.jpg",
+    name: "Bad Newz",
+    type: "Comedy",
+    language: "Hindi",
+    year: "2024",
+    duration:"2h 20min"
+  },
+  {
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQ9lpZ656p0xdUvM0hvciZlAGH8fjsSidlMg&s",
+    name: "Chhaava",
+    type: " Drama",
+    language: "Hindi",
+    year: "2025",
+    duration:"2h 41min"
+  },
+
+  {
+    image: "https://images.unsplash.com/photo-1493976040374-85c8e12f0c0e?w=400&h=600&fit=crop",
+    name: "Love in Tokyo",
+    type: "Romancs",
+    language: "English",
+    year: "2024",
+    duration:"1h 52min"
+  },
+  {
+    image: "https://images.unsplash.com/photo-1462331940025-496dfbfc7564?w=400&h=600&fit=crop",
+    name: "Beyond the Stars",
+    type: "Drama",
+    language: "English",
+    year: "2025",
+    duration:"3h 2 min"
+  },
+  {
+    image: "https://sm.ign.com/ign_in/movie/default/mv5bnzhkmjfhotitmtczni00ywm1ltlhzjitotc4ota0owvhztizxkeyxkfq_7ckm.jpg",
+    name: "Mahavatar Narsimha",
+    type: "Action",
+    language: "Tamil",
+    year: "2025",
+    duration:"2h 34min"
+  },
+  {
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7gIYVwWCZSVvwT64FLRaW3TQ-GM0RoN4Esg&s",
+    name: "Kantara",
+    type: "Action",
+    language: "Tamil",
+    year: "2025",
+    duration:"2 h 47min"
+  },
+
+
+  {
+    image: "https://upload.wikimedia.org/wikipedia/en/3/38/Paani_film_poster.jpg",
+    name: "Paani",
+    type: "Drama",
+    language: "Marathi",
+    year: "2024",
+    duration:"2h 23min"
+  },
+  {
+    image: "https://upload.wikimedia.org/wikipedia/en/6/66/Dashavatar_Marathi_movie.jpg",
+    name: "Dashavatar",
+    type: "Thriller",
+    language: "Marathi",
+    year: "2025",
+    duration:"2h 35min"
+  }
+]
+
+export default movies;
